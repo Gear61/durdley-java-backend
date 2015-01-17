eb7e8657f34c8c760a19cf7f2cde01a7865c2715
Alex Chiou <chessnone@yahoo.com>
2015-01-17 13:07:57 -0800

    Durdle.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,119 +1,67 @@
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
-// Alex's stuff
-import java.io.BufferedReader;
-import java.lang.StringBuffer;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+public class Main extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
 
-@SuppressWarnings("serial")
-public class Main extends HttpServlet
-{
-	private static final String TABLE_CREATION = "CREATE TABLE Persons (targetPhoneNumber int, description varchar(255), describerPhoneNumber int)";
-  
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-	        throws ServletException, IOException
-	{
-		resp.getWriter().print("Durdle");
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException
-	{
-		// Create table if it doesn't exist
-		Connection connection = null;
-		try
-		{
-			connection = getConnection();
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate(TABLE_CREATION);
-		}
-		catch (Exception e)
-		{
-			resp.getWriter().print("There was an error: " + e.getMessage());
-			return;
-		}
-		finally
-		{
-		  if (connection != null)
-		  {
-			  try
-			  {
-				  connection.close();
-			  }
-			  catch(SQLException e){}
-		  }
-		}
-		
-		// Read in request body (which should be a JSON)
-		StringBuffer jb = new StringBuffer();
-		String line = null;
-		try
-		{
-			BufferedReader reader = req.getReader();
-			while ((line = reader.readLine()) != null)
-			{
-				jb.append(line);
-			}
-		}
-		catch (Exception e){}
+    if (req.getRequestURI().endsWith("/db")) {
+      showDatabase(req,resp);
+    } else {
+      showHome(req,resp);
+    }
+  }
 
-		// Parse the request body into a JSON object
-		try
-		{
-			JSONObject jsonObject = new JSONObject (jb.toString());
-			if (req.getRequestURI().endsWith("/addDescriptions"))
-			{
-				addDescriptions(req, resp, jsonObject);
-			}
-		}
-		catch (JSONException e)
-		{
-			// crash and burn
-			System.out.println("JSON PARSING FAILING. WHY.");
-		}
-	}
+  private void showHome(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.getWriter().print("Hello from Java!");
+  }
 
-	private void addDescriptions(HttpServletRequest req, HttpServletResponse resp, JSONObject requestBody)
-		throws ServletException, IOException
-	{
-		resp.getWriter().print("We are adding descriptions!");
-	}
-	
+  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    try {
+      Connection connection = getConnection();
 
-	private Connection getConnection() throws URISyntaxException, SQLException
-	{
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		int port = dbUri.getPort();
+      String out = "Hello!\n";
+      while (rs.next()) {
+          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
+      }
 
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+      resp.getWriter().print(out);
+    } catch (Exception e) {
+      resp.getWriter().print("There was an error: " + e.getMessage());
+    }
+  }
 
-		return DriverManager.getConnection(dbUrl, username, password);
-	}
+  private Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	public static void main(String[] args) throws Exception
-	{
-		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		server.setHandler(context);
-		context.addServlet(new ServletHolder(new Main()),"/*");
-		server.start();
-		server.join();
-	}
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+    return DriverManager.getConnection(dbUrl, username, password);
+  }
+
+  public static void main(String[] args) throws Exception{
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new Main()),"/*");
+    server.start();
+    server.join();
+  }
 }
