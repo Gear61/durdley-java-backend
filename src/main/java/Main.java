7fc4a9c709ce2f9519ce89c4492906923ebd9c5d
Alex Chiou <chessnone@yahoo.com>
2015-01-17 13:18:57 -0800

    Durdle.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
-
-//Alex's stuff
-import java.io.BufferedReader;
-import java.lang.StringBuffer;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Main extends HttpServlet
 {
-	private static final String TABLE_CREATION = "CREATE TABLE Persons (targetPhoneNumber int, description varchar(255), describerPhoneNumber int)";
-
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException
@@ -24,62 +13,10 @@ public class Main extends HttpServlet
 		showHome(req, resp);
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException
-	{
-		// Read in request body (which should be a JSON)
-		StringBuffer jb = new StringBuffer();
-		String line = null;
-		try
-		{
-			BufferedReader reader = req.getReader();
-			while ((line = reader.readLine()) != null)
-			{
-				jb.append(line);
-			}
-		}
-		catch (Exception e)
-		{
-		}
-
-		// Parse the request body into a JSON object
-		try
-		{
-			JSONObject jsonObject = new JSONObject(jb.toString());
-			if (req.getRequestURI().endsWith("/addDescriptions"))
-			{
-				addDescriptions(req, resp, jsonObject);
-			}
-		}
-		catch (JSONException e)
-		{
-			// crash and burn
-			System.out.println("JSON PARSING FAILING. WHY.");
-		}
-	}
-
-	private void addDescriptions(HttpServletRequest req, HttpServletResponse resp,
-			JSONObject requestBody) throws ServletException, IOException
-	{
-		resp.getWriter().print("We are adding descriptions!");
-	}
-
 	private void showHome(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException
 	{
-		resp.getWriter().print("Hello from Java!");
-	}
-
-	private Connection getConnection() throws URISyntaxException, SQLException
-	{
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-		return DriverManager.getConnection(dbUrl, username, password);
+		resp.getWriter().print("Durdle");
 	}
 
 	public static void main(String[] args) throws Exception
