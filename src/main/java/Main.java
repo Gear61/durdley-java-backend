0f11d67aab7048e6d4c192b7ca4aad58d1e0c88e
Alex Chiou <chessnone@yahoo.com>
2015-01-17 12:56:22 -0800

    addDescriptions endpoint.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,56 @@
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
-
 // Alex's stuff
 import java.io.BufferedReader;
 import java.lang.StringBuffer;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@SuppressWarnings("serial")
 public class Main extends HttpServlet
 {
 	private static final String TABLE_CREATION = "CREATE TABLE Persons (targetPhoneNumber int, description varchar(255), describerPhoneNumber int)";
   
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-		throws ServletException, IOException
-	{
-		String requestUri = req.getRequestURI();
-		System.err.println(requestUri);
-	}
-  
-	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException
 	{
+		// Create table if it doesn't exist
+		Connection connection = null;
+		try
+		{
+			connection = getConnection();
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate(TABLE_CREATION);
+		}
+		catch (Exception e)
+		{
+			resp.getWriter().print("There was an error: " + e.getMessage());
+			return;
+		}
+		finally
+		{
+		  if (connection != null)
+		  {
+			  try
+			  {
+				  connection.close();
+			  }
+			  catch(SQLException e){}
+		  }
+		}
+		
+		// Read in request body (which should be a JSON)
 		StringBuffer jb = new StringBuffer();
 		String line = null;
 		try
@@ -37,59 +61,30 @@ public class Main extends HttpServlet
 				jb.append(line);
 			}
 		}
-		catch (Exception e)
-		{
-			/*report an error*/
-		}
+		catch (Exception e){}
 
+		// Parse the request body into a JSON object
 		try
 		{
-			resp.getWriter().print(jb.toString());
+			JSONObject jsonObject = new JSONObject (jb.toString());
+			if (req.getRequestURI().endsWith("/addDescription"))
+			{
+				addDescriptions(req, resp, jsonObject);
+			}
 		}
-		catch (Exception e)
+		catch (JSONException e)
 		{
+			// crash and burn
+			throw new IOException("Error parsing JSON request string");
 		}
 	}
 
-	private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+	private void addDescriptions(HttpServletRequest req, HttpServletResponse resp, JSONObject requestBody)
 		throws ServletException, IOException
 	{
-		Connection connection = null;
-		try
-		{
-			connection = getConnection();
-
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate(TABLE_CREATION);
-			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-			String out = "Hello!\n";
-			while (rs.next())
-			{
-				out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-			}
-
-			resp.getWriter().print(out);
-		}
-		catch (Exception e)
-		{
-		  resp.getWriter().print("There was an error: " + e.getMessage());
-		}
-		finally
-		{
-		  if (connection != null)
-		  {
-			  try
-			  {
-				  connection.close();
-			  }
-			  catch(SQLException e)
-			  {
-			  }
-		  }
-		}
+		resp.getWriter().print("We are adding descriptions!");
 	}
+	
 
 	private Connection getConnection() throws URISyntaxException, SQLException
 	{
